fb9f64930dd72ff8cbe8185e194eb395e1b2c019
Mikhail Kolontay <mk1981000@gmail.com>
2015-11-30 00:48:33 +0300

    Update files

---
+++ mail_checker.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# Retrieving POP3 Email with poplib
+import imaplib, getpass, email
+def main():
+    gmail = imaplib.IMAP4_SSL('imap.gmail.com', '993')
+    username = input('Login: ')
+    password = getpass.getpass()
+    gmail.login(username, password)
+    # Select the incoming unread messages
+    typ, count = gmail.select('INBOX')
+    # Displays incoming unread messages
+    typ, unseen = gmail.status('INBOX', "(UNSEEN)")
+    print (unseen)
+    # Close currently selected mailbox.
+    gmail.close()
+    # Shutdown connection to server. Returns server BYE response.
+    gmail.logout()
+main()
