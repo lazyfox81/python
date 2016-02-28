305f590c0aa3c5174d8e34ad274096c2963562a4
Mikhail Kolontay <mk1981000@gmail.com>
2016-02-28 22:56:48 +0300

    Update files

--- weather.py
+++ weather.py
@@ -8,11 +8,28 @@ import urllib.request
 import sys
 from termcolor import colored, cprint
 
+import calendar
+
 def get_weather(req_id):
     url = 'http://export.yandex.ru/weather-ng/forecasts/%s.xml' % (req_id)
     # print(url)
     tree = ET.parse(urllib.request.urlopen(url))
     return tree.getroot()
+
+def get_date(date_str):
+    """ Parsing current date from string """
+    t = date_str.split('-')
+    date = []
+    for s in t:
+        date.append(int(s))
+    return date
+
+def get_weekday(date_str):
+    """ Return day of week """
+    date_lst = get_date(date_str)
+    week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    day = week[calendar.weekday(date_lst[0], date_lst[1], date_lst[2])]
+    return day
 
 if __name__ == '__main__':
     id_city = 27612             # https://pogoda.yandex.ru/static/cities.xml
@@ -20,7 +37,7 @@ if __name__ == '__main__':
     ns = {'fc': 'http://weather.yandex.ru/forecast'}
     fact = root.find('fc:fact', ns)
     cond = fact.find('fc:weather_condition', ns)
-    cprint('Yandex.Pogoda', 'red', attrs=['bold'])
+    cprint('Yandex.Pogoda', 'magenta', attrs=['bold'])
     text = "Conditions for %s at %s" % (
         root.get('slug').capitalize(),
         fact.find('fc:observation_time', ns).text)
@@ -29,7 +46,8 @@ if __name__ == '__main__':
           (cond.get('code').capitalize(), fact.find('fc:weather_type', ns).text,
            fact.find('fc:temperature', ns).text))
     for day in root.findall('fc:day', ns):
-        cprint (day.get('date'), 'red', attrs=['bold'])
+        date_str = day.get('date')
+        cprint ((get_weekday(date_str) + " "  + date_str), 'red', attrs=['bold'])
         for day_part in day.findall('fc:day_part', ns):
             cond = day_part.find('fc:weather_condition', ns)
             temp_data = day_part.find('fc:temperature-data', ns)
